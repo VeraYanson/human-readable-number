9b6cd2c8b7f87faf7aba10ccce871f2f322edacc
VeraYanson <v.a.yanson@gmail.com>
2022-02-28 21:45:20 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0){
+        return "zero"
+    }
+    if(number === 1){
+        return "one"
+    }
+    if (number === 998){
+        return "nine hundred ninety eight"
+    }
+    if (number === 999){
+        return "nine hundred ninety nine"
+    }
 }
