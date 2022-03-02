862f2cff094d3310380dec9c085ca7fe03f856ca
VeraYanson <v.a.yanson@gmail.com>
2022-03-02 20:57:23 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -62,6 +62,258 @@ module.exports = function toReadable (number) {
     if(number === 20){
         return "twenty"
     }
+    if(number > 20 && number < 30){
+        number = number -20
+        if(number === 1){
+            return "twenty " + "one"
+        }
+        if(number === 2){
+            return "twenty " + "two"
+        }
+        if(number === 3){
+            return "twenty " + "three"
+        }
+        if(number === 4){
+            return "twenty " + "four"
+        }
+        if(number === 5){
+            return "twenty " + "five"
+        }
+        if(number === 6){
+            return "twenty " + "six"
+        }
+        if(number === 7){
+            return "twenty " + "seven"
+        }
+        if(number === 8){
+            return "twenty " + "eight"
+        }
+        if(number === 9){
+            return "twenty " + "nine"
+        }        
+    }
+    if(number > 30 && number < 40){
+        number = number - 30
+        if(number === 1){
+            return "thirty " + "one"
+        }
+        if(number === 2){
+            return "thirty " + "two"
+        }
+        if(number === 3){
+            return "thirty " + "three"
+        }
+        if(number === 4){
+            return "thirty " + "four"
+        }
+        if(number === 5){
+            return "thirty " + "five"
+        }
+        if(number === 6){
+            return "thirty " + "six"
+        }
+        if(number === 7){
+            return "thirty " + "seven"
+        }
+        if(number === 8){
+            return "thirty " + "eight"
+        }
+        if(number === 9){
+            return "thirty " + "nine"
+        }        
+    }
+    if(number > 40 && number < 50){
+        number = number - 40
+        if(number === 1){
+            return "forty " + "one"
+        }
+        if(number === 2){
+            return "forty " + "two"
+        }
+        if(number === 3){
+            return "forty " + "three"
+        }
+        if(number === 4){
+            return "forty " + "four"
+        }
+        if(number === 5){
+            return "forty " + "five"
+        }
+        if(number === 6){
+            return "forty " + "six"
+        }
+        if(number === 7){
+            return "forty " + "seven"
+        }
+        if(number === 8){
+            return "forty " + "eight"
+        }
+        if(number === 9){
+            return "forty " + "nine"
+        }        
+    }
+    if(number > 50 && number < 60){
+        number = number - 50
+        if(number === 1){
+            return "fifty " + "one"
+        }
+        if(number === 2){
+            return "fifty " + "two"
+        }
+        if(number === 3){
+            return "fifty " + "three"
+        }
+        if(number === 4){
+            return "fifty " + "four"
+        }
+        if(number === 5){
+            return "fifty " + "five"
+        }
+        if(number === 6){
+            return "fifty " + "six"
+        }
+        if(number === 7){
+            return "fifty " + "seven"
+        }
+        if(number === 8){
+            return "fifty " + "eight"
+        }
+        if(number === 9){
+            return "fifty " + "nine"
+        }        
+    }
+    if(number > 60 && number < 70){
+        number = number - 60
+        if(number === 1){
+            return "sixty " + "one"
+        }
+        if(number === 2){
+            return "sixty " + "two"
+        }
+        if(number === 3){
+            return "sixty " + "three"
+        }
+        if(number === 4){
+            return "sixty " + "four"
+        }
+        if(number === 5){
+            return "sixty " + "five"
+        }
+        if(number === 6){
+            return "sixty " + "six"
+        }
+        if(number === 7){
+            return "sixty " + "seven"
+        }
+        if(number === 8){
+            return "sixty " + "eight"
+        }
+        if(number === 9){
+            return "sixty " + "nine"
+        }        
+    }
+    if(number > 70 && number < 80){
+        number = number - 70
+        if(number === 1){
+            return "seventy " + "one"
+        }
+        if(number === 2){
+            return "seventy " + "two"
+        }
+        if(number === 3){
+            return "seventy " + "three"
+        }
+        if(number === 4){
+            return "seventy " + "four"
+        }
+        if(number === 5){
+            return "seventy " + "five"
+        }
+        if(number === 6){
+            return "seventy " + "six"
+        }
+        if(number === 7){
+            return "seventy " + "seven"
+        }
+        if(number === 8){
+            return "seventy " + "eight"
+        }
+        if(number === 9){
+            return "seventy " + "nine"
+        }        
+    }
+    if(number > 80 && number < 90){
+        number = number - 80
+        if(number === 1){
+            return "eighty " + "one"
+        }
+        if(number === 2){
+            return "eighty " + "two"
+        }
+        if(number === 3){
+            return "eighty " + "three"
+        }
+        if(number === 4){
+            return "eighty " + "four"
+        }
+        if(number === 5){
+            return "eighty " + "five"
+        }
+        if(number === 6){
+            return "eighty " + "six"
+        }
+        if(number === 7){
+            return "eighty " + "seven"
+        }
+        if(number === 8){
+            return "eighty " + "eight"
+        }
+        if(number === 9){
+            return "eighty " + "nine"
+        }        
+    }
+    if(number > 90 && number < 100){
+        number = number - 90
+        if(number === 1){
+            return "ninety " + "one"
+        }
+        if(number === 2){
+            return "ninety " + "two"
+        }
+        if(number === 3){
+            return "ninety " + "three"
+        }
+        if(number === 4){
+            return "ninety " + "four"
+        }
+        if(number === 5){
+            return "ninety " + "five"
+        }
+        if(number === 6){
+            return "ninety " + "six"
+        }
+        if(number === 7){
+            return "ninety " + "seven"
+        }
+        if(number === 8){
+            return "ninety " + "eight"
+        }
+        if(number === 9){
+            return "ninety " + "nine"
+        }        
+    }
+    if(number === 100){
+        number = number/100
+        if(number === 1){
+            return "one " + "hundred"
+        }   
+    }
+    /*if(number > 100 && number <199){
+        number = number-100
+        re = 
+        return "one hundred" + re
+    }*/
+
     if (number === 998){
         return "nine hundred ninety eight"
     }
